6340c632216da8305bdf3aa12dd184788cd8f1f6
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-11-21 11:09:49 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -149,8 +149,8 @@ char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
 
 /* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
+char wordsplit1(char *, char *);
+char wordsplit2(char *, char);
 
 /* toem_realloc.c */
 char *_memset(char *, char, unsigned int);
